98dc7e55c3ba0b6dbaadd9cc97d0e7dce385a020
Renato Vieira Santos <rvsantos_ericsson@timbrasil.com.br>
2020-03-12 16:10:55 -0300

    entrando em mavem

---
+++ arrays-java-util/src/AutoboxingUnboxing.java
@@ -0,0 +1,13 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+public class AutoboxingUnboxing {
+	 public static void main(String[] args) {
+		int[] idades =  new int[5];
+		int idade = 45;
+		List numeros = new ArrayList();
+		
+		numeross.add(idade);
+		
+	}
+}
---
+++ produtos/src/main/java/br/com/alura/maven/Produtos.java
@@ -0,0 +1,20 @@
+package br.com.alura.maven;
+
+public class Produtos {
+	private final String nome;
+	private final double preco;
+	
+	public Produtos(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	
+	
+}
